36bdbb205a7a0d4eb7b4172f09301283c20c65d3
FatihG34 <fthgnydn34@gmail.com>
2022-07-28 10:16:28 +0300

    nearly finished except style

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { TiDeleteOutline, TiEdit } from 'react-icons/ti'
 import {
     // writeUserData, getData, 
@@ -9,7 +9,10 @@ const Home = () => {
     const [name, setName] = useState('');
     const [phone, setPhone] = useState('');
     const [gender, setGender] = useState('');
-    const [user, setUser] = useState([])
+    const [user, setUser] = useState([]);
+    const [edit, setEdit] = useState(true);
+    const [itemId, setItemId] = useState()
+    const inputEdit = useRef()
 
 
     const handleSubmit = (e) => {
@@ -19,35 +22,67 @@ const Home = () => {
         setPhone('');
         setGender('Gender')
     }
-    const upDateUser = (id) => {
-        updateUser(id, name, phone, gender)
+    const upDateUser = (itemid) => {
+        setEdit(!edit)
+        setItemId(itemid)
+        inputEdit.current.focus()
+
     }
+    const handleSubmitEdit = (e) => {
+        e.preventDefault();
+        updateUser(itemId, name, phone, gender);
+        setEdit(!edit)
+    }
+
     useEffect(() => {
         getUsers(setUser)
     }, [phone])
     console.log(user);
     return (
         <div className='d-flex mt-1 gap-4 m-1'>
-            <div className='container bg-info'>
-                <form onSubmit={handleSubmit} className='d-flex flex-column p-2'>
-                    <div className="mb-3">
-                        <label htmlFor="name" className="form-label">Name</label>
-                        <input type="text" className="form-control" id="name" value={name} onChange={(e) => setName(e.target.value)} aria-describedby="name" />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="tel" className="form-label">Telephone</label>
-                        <input type="tel" className="form-control" id="tel" value={phone} onChange={(e) => setPhone(e.target.value)} />
-                    </div>
+            {edit ?
+                <div className='container bg-info'>
+                    <form onSubmit={handleSubmit} className='d-flex flex-column p-2'>
+                        <div className="mb-3">
+                            <label htmlFor="name" className="form-label">Name</label>
+                            <input type="text" className="form-control" id="name" value={name} onChange={(e) => setName(e.target.value)} aria-describedby="name" />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="tel" className="form-label">Telephone</label>
+                            <input type="tel" className="form-control" id="tel" value={phone} onChange={(e) => setPhone(e.target.value)} />
+                        </div>
 
-                    <select className="form-select" aria-label="Default select example" onChange={(e) => setGender(e.target.value)}>
-                        <option defaultValue={''}>Gender</option>
-                        <option value={'Male'}>Male</option>
-                        <option value={"Female"}>Female</option>
-                    </select>
+                        <select className="form-select" aria-label="Default select example" onChange={(e) => setGender(e.target.value)}>
+                            <option defaultValue={''}>Gender</option>
+                            <option value={'Male'}>Male</option>
+                            <option value={"Female"}>Female</option>
+                        </select>
 
-                    <button type="submit" className="btn btn-primary mt-4">Add</button>
-                </form>
-            </div>
+                        <button type="submit" className="btn btn-primary mt-4">Add</button>
+                    </form>
+                </div>
+                :
+                <div className='container bg-info'>
+                    <form onSubmit={handleSubmitEdit} className='d-flex flex-column p-2'>
+                        <div className="mb-3">
+                            <label htmlFor="name" className="form-label">Name</label>
+                            <input type="text" ref={inputEdit} className="form-control" id="name" value={name} onChange={(e) => setName(e.target.value)} aria-describedby="name" />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="tel" className="form-label">Telephone</label>
+                            <input type="tel" className="form-control" id="tel" value={phone} onChange={(e) => setPhone(e.target.value)} />
+                        </div>
+
+                        <select className="form-select" aria-label="Default select example" onChange={(e) => setGender(e.target.value)}>
+                            <option defaultValue={''}>Gender</option>
+                            <option value={'Male'}>Male</option>
+                            <option value={"Female"}>Female</option>
+                        </select>
+
+                        <button type="submit" className="btn btn-primary mt-4">Edit Your Info</button>
+                    </form>
+                </div>
+            }
             <div className='container bg-info'>
                 <table className="table">
                     <thead>
